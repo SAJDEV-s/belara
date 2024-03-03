1f824e069ad023c623eb6a20b5bd18eb03aeca01
SAJDEV <125678555+SAJDEV-full@users.noreply.github.com>
2024-03-03 20:47:14 +0330

    unique slugs

--- package/SAJDEV/belara/src/migration/2024_03_03_150108_create_blogs_table.php
+++ package/SAJDEV/belara/src/migration/2024_03_03_150108_create_blogs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->string('main_image')->nullable();
             $table->text('body');
